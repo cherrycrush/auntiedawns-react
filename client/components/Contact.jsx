9b902906f20cbb8384de7be71d7e77dfcc63cf81
cherrycrush <phobosnz@gmail.com>
2021-04-20 23:44:14 +1200

    Set up components

---
+++ client/components/Contact.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Contact () {
+  return (
+    <>
+      <main class="container">
+        <h2>Contact Details</h2>
+        <table>
+          <tr>
+            <td id="thead">Hosts</td>
+            <td>Sandra and Warren Nelmes</td>
+          </tr>
+          <tr>
+            <td id="thead">Location</td>
+            <td>Hot Water Beach (Coromandel Peninsula)</td>
+          </tr>
+          <tr>
+            <td id="thead">Postal Address</td>
+            <td>15 Radar Road, Hot Water Beach, 3591, New Zealand</td>
+          </tr>
+          <tr>
+            <td id="thead">Telephone</td>
+            <td>(64)7-866-3707</td>
+          </tr>
+          <tr>
+            <td id="thead">Mobile</td>
+            <td>021 252 6174</td>
+          </tr>
+          <tr>
+            <td id="thead">E-Mail</td>
+            <td>Sandy@AuntieDawn.co.nz</td>
+          </tr>
+        </table>
+      </main>
+    </>
+  )
+}
+
+export default Contact
