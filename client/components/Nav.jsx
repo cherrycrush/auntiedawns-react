2ffb6c9b66dce46946729374ba877d874e34adae
cherrycrush <phobosnz@gmail.com>
2021-05-04 16:18:41 +1200

    Remove bootstrap, install semantic UI

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,34 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Nav from 'react-bootstrap/Nav'
 
 function Navbar () {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navigation</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a></li>
-              <li className="nav-item">
-                <a className="nav-link" href="hotwaterbeach.html">Hot Water Beach</a></li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Accommodation</a></li>
-              <li className="nav-item">
-                <Link to='/contact'><button className="btn btn-outline-secondary">Contact Us</button></Link></li>
-              <li className="facebook">
-                <a className="nav-link" href="https://www.facebook.com/auntiedawnsbedandbreakfast"><img src="img/facebook.png" alt="Our Facebook" width="32" height="32" /></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
     </>
   )
 }
