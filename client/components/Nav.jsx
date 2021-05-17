6cadfd441c946a3a6276c5ef0aba665c984a2dfc
cherrycrush <phobosnz@gmail.com>
2021-05-17 23:59:14 +1200

    Add semantic ui

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
 
-function Navbar () {
-  return (
-    <>
-    </>
-  )
+export default class MenuExampleInvertedSecondary extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Segment>
+        <Menu pointing secondary>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Segment>
+    )
+  }
 }
-
-export default Navbar
