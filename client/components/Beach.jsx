4f8b30566f6ed7af07b80c033f40df77b69e7ff0
cherrycrush <phobosnz@gmail.com>
2021-06-14 23:35:10 +1200

    Add react boilerplate for Beach

---
+++ client/components/Beach.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Beach () {
+  return (
+    <>
+      <div className="head-container">
+        <h1>Auntie Dawn's Place -  Hot Water Beach</h1>
+      </div>
+    </>
+  )
+}
+
+export default Beach
